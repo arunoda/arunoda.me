3ceeea5399f51dffe8bcca730fd5bc051feb06cf
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-06-19 19:38:59 +0530

    Add SSR caching.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const next = require('next')
 const MongoClient = require('mongodb').MongoClient
+const LRUCache = require('lru-cache')
 const Usage = require('./usage')
 
 const mongoUrl = process.env.MONGO_URL
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const pageCache = new LRUCache({ max: 500 })
+
+function renderPost(app, req, res) {
+  if (dev) {
+    handle(req, res)
+    return
+  }
+
+  if (pageCache.has(req.path)) {
+    res.send(pageCache.get(req.path))
+    return
+  }
+
+  app
+    .renderToHTML(req, res, req.path, req.query)
+    .then(html => {
+      // Let's cache this page
+      // eslint-disable-next-line no-console
+      console.log(`CACHE MISS: ${req.path}`);
+      pageCache.set(req.path, html);
+
+      // send it
+      res.send(html);
+    })
+    .catch(err => {
+      console.error(err.stack)
+      app.renderError(err, req, res, req.path, req.query);
+    });
+}
 
 async function run() {
   let db
@@ -25,7 +55,7 @@ async function run() {
   })
 
   server.get('/blog/:slug', (req, res) => {
-    handle(req, res)
+    renderPost(app, req, res)
     if (db) Usage.trackPost(db, req.url, 'view')
   })
 
