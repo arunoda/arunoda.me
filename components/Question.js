d36a98ed65195ce1a2aa827028b47063a3f33308
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-07-23 17:29:58 +0530

    Add some style changes.

--- components/Question.js
+++ components/Question.js
@@ -5,11 +5,11 @@ const Button = ({ onClick, children }) => (
     { children }
     <style jsx>{`
       button {
-        background-color: #03A9F4;
+        background-color: #D84315;
         border-radius: 3px;
         border: 2px solid #FFF;
         color: #FFF;
-        padding: 5px 10px;
+        padding: 5px 20px;
         font-size: 16px;
         cursor: pointer;
       }
@@ -23,13 +23,21 @@ const Button = ({ onClick, children }) => (
 
 export default class Question extends React.Component {
   state = {
-    givenAnswer: null
+    givenAnswer: null,
+    showAll: false
   }
 
   reset () {
     this.setState({
+      showAll: false,
       givenAnswer: null,
       currentAnswer: null
+    })
+  }
+
+  showAll (action = true) {
+    this.setState({
+      showAll: action
     })
   }
 
@@ -39,6 +47,11 @@ export default class Question extends React.Component {
 
   submitAnswer () {
     const { currentAnswer } = this.state
+    if (!currentAnswer) {
+      alert('Select an answer.')
+      return
+    }
+
     this.setState({
       givenAnswer: currentAnswer,
       currentAnswer: null
@@ -48,22 +61,45 @@ export default class Question extends React.Component {
   renderQuestions () {
     const { answers } = this.props
     return (
-      <div>
+      <div className="answer-box">
         <div className="answers">
           {answers.map((answer) => (
-            <div key={answer}>
+            <div className="answer" key={answer}>
               <input
                 type="radio"
                 name="answer"
                 value={answer}
                 onChange={() => this.setAnswer(answer)}
               />
-              <span>{answer}</span>
+              <div className="text">{answer}</div>
+              <div className="clearfix" />
             </div>
           ))}
         </div>
         <Button onClick={() => this.submitAnswer()}>Submit</Button>
         <style jsx>{`
+          .answer {
+            position: relative;
+          }
+
+          .answer input {
+            position: absolute;
+            left: 0;
+            top: 5px;
+          }
+
+          .answer .text {
+            margin-left: 25px;
+          }
+
+          .clearfix {
+            clear: both;
+          }
+
+          .answer-box {
+            margin: 15px 0 0 0;
+          }
+
           .answers {
             margin: 0 0 10px 0;
           }
@@ -78,7 +114,7 @@ export default class Question extends React.Component {
     const isCorrect = correctAnswer === givenAnswer 
 
     return (
-      <div>
+      <div className="result-box">
         { isCorrect? (
           <div className="correct">Yes. You are correct :)</div>
         ) : (
@@ -88,13 +124,22 @@ export default class Question extends React.Component {
           <h4>Correct Answer</h4>
           <div>{correctAnswer}</div>
         </div>
-        <Button onClick={() => this.reset()}>Answer Again</Button>
+        <div className="reset" onClick={() => this.reset()}>( Click here to reset )</div>
         <style jsx>{`
+          .result-box {
+            margin: 15px 0 0 0;
+          }
+
           h4 {
             margin: 10px 0 0 0;
             padding: 0;
             font-size: 18px;
             font-weight: 600;
+          }
+
+          .reset {
+            cursor: pointer;
+            font-size: 12px;
           }
 
           .correct-answer {
@@ -105,24 +150,41 @@ export default class Question extends React.Component {
     )
   }
 
+  renderDetails () {
+    const { givenAnswer } = this.state
+    return givenAnswer? this.renderResult() : this.renderQuestions()
+  }
+
+  showClickToAnswer () {
+    return (
+      <div onClick={() => this.showAll() }>
+        ( Click here to answer )
+        <style jsx>{`
+          div {
+            cursor: pointer;
+            font-size: 12px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+
   render () {
     const { question } = this.props
-    const { givenAnswer } = this.state
+    const { showAll } = this.state
 
     return (
-      <div className="question">
-        <h2>Q: {question}</h2>
-        { givenAnswer? this.renderResult() : this.renderQuestions() }
+      <div className="question-box">
+        <div className="question">Q: {question}</div>
+        { showAll? this.renderDetails() : this.showClickToAnswer() }
         <style jsx>{`
-          .question {
+          .question-box {
             background-color: #3f51b5;
-            padding: 15px;
+            padding: 15px 15px;
             color: #FFF;
           }
 
-          h2 {
-            margin: 0 0 15px 0;
-            padding: 0;
+          .question {
             font-size: 20px;
             font-weight: 400;
           }
