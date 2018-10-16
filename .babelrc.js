3bb6286e78a21ebaabf30ccde2a500c382c59a2c
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2018-10-17 02:21:18 +0530

    Add .babelrc.js instead of .babelrc transform-define plugin doesn't work with .babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,11 @@
-{
+const env = require("./env-config.js");
+
+module.exports = {
   "presets": [
     "next/babel"
   ],
   "plugins": [
-    [
-      "transform-define",
-      "./env-config.js"
-    ],
+    ["transform-define", env],
     "markdown-in-js/babel",
     "babel-plugin-root-import"
   ]
