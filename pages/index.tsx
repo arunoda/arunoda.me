3f0c029e2e7b49805dce3d62a890e8da29bb4f8e
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2026-02-22 21:15:29 +0530

    Improve the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Head from 'next/head'
 import { generateNextSeo as NextSeo } from 'next-seo/pages'
 import styles from './index.module.css'
+import posts from '../posts'
 
 const MainLink = ({ href, as, children, nomargin }: any) => (
   <span className={nomargin ? styles.spanNoMargin : styles.span}>
@@ -16,6 +17,8 @@ const MainLink = ({ href, as, children, nomargin }: any) => (
 )
 
 export default function Index() {
+  const latestPosts = posts.slice(0, 5)
+
   return (
     <div className={styles.wrapper}>
       <Head>
@@ -24,7 +27,7 @@ export default function Index() {
           description: "Arunoda Susiripala's personal space on the internet."
         })}
       </Head>
-      <div>
+      <div className={styles.content}>
         <div className={styles.intro}>
           <h1 className={styles.h1}>Arunoda Susiripala</h1>
         </div>
@@ -32,6 +35,24 @@ export default function Index() {
           <MainLink href='https://twitter.com/arunoda'>Twitter</MainLink>
           <MainLink href='/blog'>Blog</MainLink>
           <MainLink href='https://github.com/arunoda'>GitHub</MainLink>
+        </div>
+        <div className={styles.latestPosts}>
+          <h2 className={styles.latestPostsTitle}>Latest Posts</h2>
+          <ul className={styles.latestPostsList}>
+            {latestPosts.map((post) => (
+              <li key={post.slug} className={styles.postItem}>
+                <span className={styles.postDate}>{post.date}</span>
+                <Link href={`/blog/${post.slug}`} legacyBehavior>
+                  <a className={styles.postLink}>{post.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.seeAll}>
+            <Link href='/blog' legacyBehavior>
+              <a className={styles.seeAllLink}>See all posts</a>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
